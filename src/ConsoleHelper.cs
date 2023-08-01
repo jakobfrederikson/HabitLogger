a2b12804eede829b326106368b2187342be8b5ea
Jakob <jakobfred1999@gmail.com>
2023-08-02 11:48:44 +1200

    Console helper now handles user input for creating and editing a new habit.

---
+++ src/ConsoleHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitLogger.src;
+
+public class ConsoleHelper
+{
+	public static void CreateMenu(string menuTitle, string[] menuOptions, string exitOption = "Exit")
+	{
+        Console.WriteLine(menuTitle);
+        for (int i = 0; i < menuOptions.Length; i++)
+		{
+            Console.WriteLine($"[{i + 1}] {menuOptions[i]}");
+        }
+		Console.WriteLine($"\n[0] {exitOption}");
+	}
+
+	public static string GetValidString(string message)
+	{
+		Console.Write(message);
+
+		string response = Console.ReadLine();
+
+		if (response == "")
+		{
+			while (response == "")
+			{
+                Console.Write("Please enter a non-empty string: ");
+				response = Console.ReadLine();
+            }
+		}
+
+		return response;
+	}
+
+	public static int GetInt(string message)
+	{
+		int number = 0;
+
+        Console.Write(message);
+		while(!int.TryParse(Console.ReadLine(), out number))
+		{
+            Console.WriteLine($"{number} is not a number. Enter a valid id: ");
+        }
+
+		return number;
+    }
+
+	public static bool Confirm(string message)
+	{
+		ConsoleKey response;
+
+		do
+		{
+			Console.Write($"{message} [y/n] ");
+			response = Console.ReadKey(false).Key;
+			if (response != ConsoleKey.Enter)
+				Console.WriteLine();
+		} while (response != ConsoleKey.Y && response != ConsoleKey.N);
+
+		return response == ConsoleKey.Y;
+    }
+}
--- src/ConsoleManager.cs
+++ src/ConsoleManager.cs
@@ -22,90 +22,134 @@ public class ConsoleManager
     {
         while (RunApp)
         {
-			MainMenu();
-
-            string response = Console.ReadLine();
-
-            switch (response)
+            switch (mainMenu())
             {
                 case "0":
                     RunApp = false;
                     break;
                 case "1":
+                    createHabit();
                     break;
                 case "2":
+                    editHabit();
                     break;
                 case "3":
+                    deleteHabit();
                     break;
                 case "4":
-                    break;
+					habitLogMenu();
+					break;
                 case "5":
-                    HabitLogMenu();
+                    break;
+                case "6":
+                    break;
+                case "7":
                     break;
                 default:
                     break;
             }
-
-            Console.Clear();
 		}
     }
 
     private void Title()
     {
+        Console.Clear();
 		Console.WriteLine("Habit Logger");
 		Console.WriteLine("------------\n");
 	}
 
-    private void MainMenu()
+    private string mainMenu()
     {
         Title();
-
-        /* 
-         TODO:
-        If Habits table is EMPTY:
-            cw."You have no habits logged. Enter '1' to create a new habit."
-        else:
-            Display all habits
-            cw."Select a habit: "
-
-        If Habit selected:
-            cw."HABIT NAME"
-            Display all logs for this habit
-            cw."View habit options"
-            cw."View log options"
-            cw."Back"        
-         */
 
         if (dbManager.Habits.Empty)
-            Console.WriteLine("Habits is empty");
+        {
+            Console.WriteLine("You haven't created any habits yet.\n");
+            Console.WriteLine("[1] Create a new habit\n");
+        }
         else
-            Console.WriteLine("Habits has data");
-
-		if (dbManager.HabitsTracker.Empty)
-			Console.WriteLine("HabitsTracker is empty");
-		else
-			Console.WriteLine("HabitsTracker has data");
-
-		Console.WriteLine("Select an option\n");
-
-        Console.WriteLine("[1] Create a new habit");
-        Console.WriteLine("[2] Log a habit");
-        Console.WriteLine("[3] ");
-
-        Console.WriteLine("[0] To exit");
-		Console.Write("What option do you choose? ");
+        {
+            dbManager.Habits.Read();
+			Console.WriteLine("[1] Create a new habit");
+            Console.WriteLine("[2] Edit a habit");
+            Console.WriteLine("[3] Delete a habit\n");
+            Console.WriteLine("[4] Create a log");
+            Console.WriteLine("[5] View logs");
+            Console.WriteLine("[6] Edit a log");
+            Console.WriteLine("[7] Delete a log");
+        }
+		Console.WriteLine("\n[0] Exit");
+		Console.Write("\nSelect an option: ");
+        return Console.ReadLine();
 	}
 
-    private void HabitLogMenu()
+    private void createHabit()
     {
         Title();
-		Console.WriteLine("Select an option");
-		Console.WriteLine("[0] To exit");
-		Console.WriteLine("[1] Create a habit log");
-		Console.WriteLine("[2] View all habits");
-		Console.WriteLine("[3] Edit a habit");
-		Console.WriteLine("[4] Delete a habit");
-		Console.WriteLine("[5] Create a habit log");
-		Console.Write("What option do you choose? ");
+        string? habitName = ConsoleHelper.GetValidString("Enter name of the habit to create: ");
+        dbManager.Habits.Create(habitName);
+    }
+
+    private void editHabit()
+    {
+        Title();
+        dbManager.Habits.Read();
+        string? response = ConsoleHelper.GetValidString("Write the id of the habit you want to edit: ");
+        int id = 0;
+		int.TryParse(response, out id);
+
+		ConsoleHelper.CreateMenu($"Edit habit #{response}", new string[] { "Name", "Date" }, "Back");
+		switch (ConsoleHelper.GetValidString("Select an option: "))
+        {
+            case "1":
+                string nameString = ConsoleHelper.GetValidString("Enter new name: ");
+				dbManager.Habits.Update(id, nameString, UpdateOptions.Hname);
+                break;
+            case "2":
+				string updateString = ConsoleHelper.GetValidString("Enter new Date (d/mm/yyyy): ");
+				dbManager.Habits.Update(id, updateString, UpdateOptions.Date);
+                break;
+            case "0":
+                return;
+            default:
+                break;
+        }
+    }
+
+    private void deleteHabit()
+    {
+        Title();
+        dbManager.Habits.Read();
+        int id = ConsoleHelper.GetInt("Enter the id of the habit you wish to delete: ");
+
+        if (ConsoleHelper.Confirm("Are you sure you want to delete this habit?"))
+        {
+            Console.WriteLine($"Deleting habit of ID: {id}");
+            dbManager.Habits.Delete(id);
+            Console.Write("Press any key to continue. . .");
+            Console.ReadKey(false);
+		}
+    }
+
+    private void habitLogMenu()
+    {
+		Title();
+		if (dbManager.HabitsTracker.Empty)
+        {
+            Console.WriteLine("You haven't logged any of your habits yet.\n");
+            Console.WriteLine("[1] Create a new log");
+            
+        }
+        else
+        {
+            dbManager.HabitsTracker.Read();
+			Console.WriteLine("[1] Create a new log");
+			Console.WriteLine("[2] Edit a log");
+			Console.WriteLine("[3] Delete a log");
+            Console.WriteLine("[4] View logs for a specific habit");
+		}
+        Console.WriteLine("\n[0] Back to main menu");
+        
+        Console.ReadLine();
 	}
 }
