5d8cc54b3438ab054495ca4bef1cd19925e51f8b
Jakob <jakobfred1999@gmail.com>
2023-08-03 16:03:12 +1200

    Commenting out interface for now

--- src/Database/IHabitLoggerTable.cs
+++ src/Database/IHabitLoggerTable.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 
 namespace HabitLogger.src.Database;
 
-public enum UpdateOptions { None, Date, Hname, Tquantity, TforeignKey }
-public interface IHabitLoggerTable
-{
-    public string? Filename { get; set; }
-    public bool Empty { get; }
-    void CreateTableIfNotExists();
-    void Create(string habitName = "default", int habitId = 0, int habitQuantity = 0);
-    void Read();
-    void Update(int id, string updateString, UpdateOptions updateOption);
-    void Delete(int id);
-}
+//public enum UpdateOptions { None, Date, Hname, Tquantity, TforeignKey }
+//public interface IHabitLoggerTable
+//{
+//    public string? Filename { get; set; }
+//    public bool Empty { get; }
+//    void CreateTableIfNotExists();
+//    void Create(string habitName = "default", int habitId = 0, int habitQuantity = 0);
+//    void Read();
+//    void Update(int id, string updateString, UpdateOptions updateOption);
+//    void Delete(int id);
+//}
